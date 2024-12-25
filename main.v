e6ebaae45ba42a6cff15f50ab3826d97baf2de9d
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-12-25 22:23:59 +0100

    Initial commit
    Add command line parsing.

---
+++ main.v
@@ -0,0 +1,28 @@
+module main
+
+import flag
+import os
+
+fn main() {
+	mut fp := flag.new_flag_parser(os.args)
+
+	fp.application('geo')
+	fp.version('0.0.1')
+	fp.description('A sample CLI application that prints geometric shapes to the console.')
+	fp.skip_executable()
+
+	shape := fp.string('shape', `p`, 'none', 'The shape to use for the geometry.').to_lower()
+	size := fp.int('size', `z`, 5, 'The size parameter for the shapes.')
+	symbol := fp.string('symbol', `m`, '*', 'The symbol to use for the geometry.').runes()[0] or {
+		`*`
+	}
+	additional_args := fp.finalize()!
+
+	if additional_args.len > 0 {
+		println('Unprocessed arguments:\n${additional_args.join_lines()}')
+	}
+
+	println(shape)
+	println(size)
+	println(symbol)
+}
