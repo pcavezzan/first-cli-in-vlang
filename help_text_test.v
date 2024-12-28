1400ceadec6d4e0abff062b5c661a0af71086ad2
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-12-28 10:53:12 +0100

    Add tests.

---
+++ help_text_test.v
@@ -0,0 +1,27 @@
+module main
+
+import os
+
+const output_search_terms = [
+	'Usage',
+	'Description',
+	'Options',
+	'-p, --shape <string>',
+	'-z, --size <int>',
+	'-m, --symbol <string>',
+	'-h, --help',
+	'--version',
+]
+
+fn test_help() {
+	flags := ['--help', '-h']
+
+	for flag in flags {
+		result := os.execute_or_panic('${os.quoted_path(@VEXE)} run . ${flag}')
+
+		assert result.exit_code == 0
+		for term in output_search_terms {
+			assert result.output.contains(term)
+		}
+	}
+}
---
+++ multi_option_test.v
@@ -0,0 +1,63 @@
+module main
+
+import os
+
+struct RunConfig {
+	shape  string
+	size   int    = 5
+	symbol string = '*'
+	output []string
+}
+
+const all_options_test_cases = [
+	RunConfig{
+		shape:  'square'
+		size:   5
+		symbol: '|'
+		output: ['|||||', '|||||', '|||||', '|||||', '|||||']
+	},
+	RunConfig{
+		shape:  'pyramid'
+		size:   3
+		symbol: '^'
+		output: ['  ^', ' ^^^', '^^^^^']
+	},
+	RunConfig{
+		shape:  'diamond'
+		size:   4
+		symbol: '*'
+		output: ['   *', '  ***', ' *****', '*******', ' *****', '  ***', '   *']
+	},
+]
+const shape_only_test_cases = [
+	RunConfig{
+		shape:  'square'
+		output: ['*****', '*****', '*****', '*****', '*****']
+	},
+	RunConfig{
+		shape:  'pyramid'
+		output: ['    *', '   ***', '  *****', ' *******', '*********']
+	},
+	RunConfig{
+		shape:  'left-triangle'
+		output: ['*', '**', '***', '****', '*****']
+	},
+]
+
+fn test_all_options() {
+	for case in all_options_test_cases {
+		result := os.execute_or_panic('${os.quoted_path(@VEXE)} run . --shape ${case.shape} --size ${case.size} --symbol "${case.symbol}"')
+
+		assert result.exit_code == 0
+		assert result.output.split_into_lines() == case.output
+	}
+}
+
+fn test_shapes_only() {
+	for case in shape_only_test_cases {
+		result := os.execute_or_panic('${os.quoted_path(@VEXE)} run . --shape ${case.shape}')
+
+		assert result.exit_code == 0
+		assert result.output.split_into_lines() == case.output
+	}
+}
